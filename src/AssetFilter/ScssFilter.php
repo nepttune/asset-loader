c71178de3ec20b19e96f836a773cfd349ea86304
Václav Pelíšek <peldax@gmail.com>
2019-01-13 17:48:16 +0100

    Create ScssFilter.php

---
+++ src/AssetFilter/ScssFilter.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of Nepttune (https://www.peldax.com)
+ *
+ * Copyright (c) 2018 Václav Pelíšek (info@peldax.com)
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <https://www.peldax.com>.
+ */
+
+namespace Nepttune\AssetFilter;
+
+use \WebLoader\Compiler;
+
+class ScssFilter
+{
+    /**
+     * Compile target code
+     * @param string $code
+     * @param Compiler $compiler
+     * @return string
+     */
+    public function __invoke($code, Compiler $compiler)
+    {
+        $compiler = new \Leafo\ScssPhp\Compiler();
+        return $compiler->compile($code);
+    }
+}
