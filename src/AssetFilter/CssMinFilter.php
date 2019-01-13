90f0fcefbd0ee8c870d1711bba59b4e3b588d9ce
Václav Pelíšek <peldax@gmail.com>
2019-01-13 17:50:44 +0100

    Create CssMinFilter.php

---
+++ src/AssetFilter/CssMinFilter.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of Nepttune (https://www.peldax.com)
+ *
+ * Copyright (c) 2018 Václav Pelíšek (info@peldax.com)
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <https://www.peldax.com>.
+ */
+
+declare(strict_types = 1);
+
+namespace Nepttune\AssetFilter;
+
+use \WebLoader\Compiler;
+
+class CssMinFilter
+{
+    /**
+     * Minify target code
+     * @param string $code
+     * @param Compiler $compiler
+     * @return string
+     */
+    public function __invoke($code, Compiler $compiler)
+    {
+        $minifier = new \MatthiasMullie\Minify\CSS($code);
+        return $minifier->minify();
+    }
+}
